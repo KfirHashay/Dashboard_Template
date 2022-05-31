bd0db947b647fc2a845c7762d840de80c33a78a6
Kfir Hashay <Kfirhashay@outlook.com>
2022-05-31 11:13:16 +0300

    Pages Fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,22 @@
 import React, { useEffect } from "react";
-
-// Icons
 import { AiOutlineMenu } from "react-icons/ai";
 import { FiShoppingCart } from "react-icons/fi";
 import { BsChatLeft } from "react-icons/bs";
 import { RiNotification3Line } from "react-icons/ri";
 import { MdKeyboardArrowDown } from "react-icons/md";
-// Icons End
-
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import avatar from "../data/avatar.jpg";
-
 import { Cart, Chat, Notification, UserProfile } from ".";
-
 import { useStateContext } from "../contexts/ContextProvider";
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   <TooltipComponent content={title} position="BottomCenter">
     <button
       type="button"
-      onClick={customFunc}
+      onClick={() => customFunc()}
       style={{ color }}
-      className="relative text-xl rounded-full p-3 hover:bg=light-gray"
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
     >
       <span
         style={{ background: dotColor }}
@@ -35,23 +29,23 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 const Navbar = () => {
   const {
+    currentColor,
     activeMenu,
     setActiveMenu,
-    isClicked,
-    setIsClicked,
     handleClick,
+    isClicked,
+    setScreenSize,
     screenSize,
-    setscreenSize,
   } = useStateContext();
 
   useEffect(() => {
-    const handleResize = () => setscreenSize(window.innerWidth);
+    const handleResize = () => setScreenSize(window.innerWidth);
 
     window.addEventListener("resize", handleResize);
+
     handleResize();
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   useEffect(() => {
@@ -60,38 +54,37 @@ const Navbar = () => {
     } else {
       setActiveMenu(true);
     }
+  }, [screenSize]);
 
-    return () => {};
-  }, [screenSize, setActiveMenu]);
+  const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
-    <div className="flex justify-between p-2 md:mx-6 relative">
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
       <NavButton
         title="Menu"
-        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
-        color="blue"
+        customFunc={handleActiveMenu}
+        color={currentColor}
         icon={<AiOutlineMenu />}
       />
-
       <div className="flex">
         <NavButton
           title="Cart"
           customFunc={() => handleClick("cart")}
-          color="blue"
+          color={currentColor}
           icon={<FiShoppingCart />}
         />
         <NavButton
           title="Chat"
-          customFunc={() => handleClick("chat")}
-          color="blue"
           dotColor="#03C9D7"
+          customFunc={() => handleClick("chat")}
+          color={currentColor}
           icon={<BsChatLeft />}
         />
         <NavButton
-          title="Notifications"
-          customFunc={() => handleClick("Notification")}
-          color="blue"
-          dotColor="#03C9D7"
+          title="Notification"
+          dotColor="rgb(254, 201, 15)"
+          customFunc={() => handleClick("notification")}
+          color={currentColor}
           icon={<RiNotification3Line />}
         />
         <TooltipComponent content="Profile" position="BottomCenter">
@@ -99,9 +92,13 @@ const Navbar = () => {
             className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
             onClick={() => handleClick("userProfile")}
           >
-            <img className="rounded-full w-8 h-8" alt="" src={avatar} />
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
             <p>
-              <span className="text-gray-400 text-14">Hi, </span>{" "}
+              <span className="text-gray-400 text-14">Hi,</span>{" "}
               <span className="text-gray-400 font-bold ml-1 text-14">
                 Michael
               </span>
